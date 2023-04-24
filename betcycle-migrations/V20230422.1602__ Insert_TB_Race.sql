a38f989e767d390ebf3ac5c1061b579b637ef3c4
eduardopiairo <eduardopiairo@gmail.com>
2023-04-25 00:04:26 +0100

    update build db

---
+++ betcycle-migrations/V20230422.1602__ Insert_TB_Race.sql
@@ -0,0 +1,3 @@
+INSERT INTO Race(RaceName, RaceYear, RaceStartDate, RaceEndDate) VALUES('Giro d''Italia', 2023, '2023-05-06', '2023-05-28');
+INSERT INTO Race(RaceName, RaceYear, RaceStartDate, RaceEndDate) VALUES('Tour de France', 2023, '2023-07-01', '2023-07-23');
+INSERT INTO Race(RaceName, RaceYear, RaceStartDate, RaceEndDate) VALUES('La Vuelta', 2023, '2023-08-26', '2023-09-17');

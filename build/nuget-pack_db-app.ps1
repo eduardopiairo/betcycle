8e4795e7d04e6431e9aeb654b563aa426f24b1ea
eduardopiairo <eduardopiairo@gmail.com>
2023-04-26 23:24:43 +0100

    update create db pack

--- build/nuget-pack_db-app.ps1
+++ build/nuget-pack_db-app.ps1
@@ -2,6 +2,6 @@ Param(
 	[Parameter(Mandatory=$true)][string]$resourceName,
 	[Parameter(Mandatory=$true)][string]$buildNumber)
 
-nuget pack .\$resourceName\publish\$resourceName.nuspec -Version $buildNumber -BasePath .\src\DevOpsPorto\bin\Release\netcoreapp1.0\publish -OutputDirectory ..\output
+nuget pack .\$resourceName\publish\$resourceName.nuspec -Version $buildNumber -BasePath .\$resourceName -OutputDirectory .\output
 
 exit $LastExitCode

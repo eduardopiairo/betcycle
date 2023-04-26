0c568c2e9fb19a9a435e328ed9c8ed2181057b7b
eduardopiairo <eduardopiairo@gmail.com>
2023-04-27 00:57:26 +0100

    test pack version

--- build/build_db_nuget_pack.ps1
+++ build/build_db_nuget_pack.ps1
@@ -4,6 +4,6 @@ Param(
 
 Write-Output $buildNumber	
 
-nuget pack .\$resourceName\$resourceName.nuspec -Version 1.1.1 -BasePath .\$resourceName -OutputDirectory .\output
+nuget pack .\$resourceName\$resourceName.nuspec -Version 1.1 -BasePath .\$resourceName -OutputDirectory .\output
 
 exit $LastExitCode

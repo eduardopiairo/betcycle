08e75029e6335e5c67fda6bb09ac3abeee2d0368
eduardopiairo <eduardopiairo@gmail.com>
2023-04-26 23:27:25 +0100

    create pack step

--- build/build_db_nuget_pack.ps1
+++ build/build_db_nuget_pack.ps1
@@ -2,6 +2,6 @@ Param(
 	[Parameter(Mandatory=$true)][string]$resourceName,
 	[Parameter(Mandatory=$true)][string]$buildNumber)
 
-nuget pack .\$resourceName\publish\$resourceName.nuspec -Version $buildNumber -BasePath .\$resourceName -OutputDirectory .\output
+nuget pack .\$resourceName\$resourceName.nuspec -Version $buildNumber -BasePath .\$resourceName -OutputDirectory .\output
 
 exit $LastExitCode

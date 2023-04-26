4b5e9e20f56ac27f1d6dcb1b71f85f81c0a7a4c1
eduardopiairo <eduardopiairo@gmail.com>
2023-04-27 00:24:12 +0100

    push step

--- build/build_db_nuget_pack.ps1
+++ build/build_db_nuget_pack.ps1
@@ -2,6 +2,8 @@ Param(
 	[Parameter(Mandatory=$true)][string]$resourceName,
 	[Parameter(Mandatory=$true)][string]$buildNumber)
 
+Write-Output $buildNumber	
+
 nuget pack .\$resourceName\$resourceName.nuspec -Version $buildNumber -BasePath .\$resourceName -OutputDirectory .\output
 
 exit $LastExitCode

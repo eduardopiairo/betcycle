649bc1bc04ce555711d4ebe885e67df750eac1a2
eduardopiairo <eduardopiairo@gmail.com>
2023-04-27 01:29:46 +0100

    push to octo

--- build/build_db_nuget_push.ps1
+++ build/build_db_nuget_push.ps1
@@ -10,7 +10,9 @@ $packFullPath = $packPath + "." +$buildNumber + $packExtension
 Write-Output $packFullPath
 
 #push from VSTS to Octopus Deploy
-nuget push $packFullPath -Source http://localhost:8080/nuget/packages -ApiKey API-OCDNB3OLYYRC4FSMQKPVYCVO1KXXPUN
+#nuget push $packFullPath -Source http://localhost:8080/nuget/packages -ApiKey API-OCDNB3OLYYRC4FSMQKPVYCVO1KXXPUN
+
+nuget push $packFullPath -ApiKey API-OCDNB3OLYYRC4FSMQKPVYCVO1KXXPUN -Source http://localhost:8080/Spaces-1/nuget/packages
 
 
 exit $LastExitCode

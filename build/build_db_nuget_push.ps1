b26c4bcd48cd34a4fe2d9317ed07b5623cb1e4de
eduardopiairo <eduardopiairo@gmail.com>
2023-04-27 01:22:25 +0100

    push to octo

--- build/build_db_nuget_push.ps1
+++ build/build_db_nuget_push.ps1
@@ -13,8 +13,4 @@ Write-Output $packFullPath
 nuget push $packFullPath -Source http://localhost:8080/default/nuget/packages -ApiKey API-OCDNB3OLYYRC4FSMQKPVYCVO1KXXPUN
 
 
-
-nuget.exe push -Source https://pkgs.dev.azure.com/eduardopiairo/betcycle/_packaging/betcycle-artifacts/nuget/v3 -ApiKey az $packFullPath
-
-
 exit $LastExitCode

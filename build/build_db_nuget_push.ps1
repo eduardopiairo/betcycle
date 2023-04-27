e1f1c1f28387dead8655e98f72e7caf38d4870e6
eduardopiairo <eduardopiairo@gmail.com>
2023-04-27 01:19:18 +0100

    push to octo

--- build/build_db_nuget_push.ps1
+++ build/build_db_nuget_push.ps1
@@ -10,7 +10,8 @@ $packFullPath = $packPath + "." +$buildNumber + $packExtension
 Write-Output $packFullPath
 
 #push from VSTS to Octopus Deploy
-#nuget push $packFullPath -Source http://localhost:8090/nuget/packages -ApiKey $apiKey
+nuget push $packFullPath -Source http://localhost:8080/default/nuget/packages -ApiKey API-OCDNB3OLYYRC4FSMQKPVYCVO1KXXPUN
+
 
 
 nuget.exe push -Source https://pkgs.dev.azure.com/eduardopiairo/betcycle/_packaging/betcycle-artifacts/nuget/v3 -ApiKey az $packFullPath

a8103900fa7ded49aafd5f6a62453ae57afc93ad
eduardopiairo <eduardopiairo@gmail.com>
2023-12-12 00:19:29 +0000

    remove flyway

--- flyway-9.16.3/sql/V20230606.1600__Create_TB_Race.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE race
-(
-    race_id     SERIAL PRIMARY KEY,
-    race_name   VARCHAR(50) NOT NULL,
-    race_year   INT NULL,
-    race_start_date DATE NULL,
-    race_end_date DATE NULL
-);
-

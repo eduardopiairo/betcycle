965688c16f10170e5945ffa6f6de0d1ba65bd24a
eduardopiairo <eduardopiairo@gmail.com>
2025-03-16 17:50:26 +0000

    remove octopus deploy

--- octopus-deploy/octo-pack.ps1
+++
@@ -1,8 +0,0 @@
-Param(
-	[Parameter(Mandatory=$true)][string]$resourceName,
-	[Parameter(Mandatory=$true)][string]$buildNumber)
-	
-
-octo pack --id=$resourceName --version=$buildNumber --format=zip --basePath=./src/DevOpsPorto/bin/Release/netcoreapp1.0/publish
-
-exit $LastExitCode
